9a40d65ad94300c4ccc24d0cbc36c619f7d32a28
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-22 16:54:20 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,15 @@
 class Book
 
-  def initialize(title, author, page_count)
+  def initialize(title)
     @title = title
-    @author = author
-    @page_count = page_count
   end
 
   def title
     @title
   end
 
-  def author
-    @author
+  def author=(author)
+    @author = author
   end
 
-  def page_count
-    @page_count
-  end
 end
